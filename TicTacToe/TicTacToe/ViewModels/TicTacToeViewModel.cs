81848dbbde2eaf46c917ec08117ad7eca8572f6b
elegiya <marina.a.gaponova@gmail.com>
2015-10-08 15:51:49 +0300

    Adding buttons on UI, styles, main classes for architectual vision (made on lesson)

---
+++ TicTacToe/TicTacToe/ViewModels/TicTacToeViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Acr.UserDialogs;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TicTacToe.Services;
+using Xamarin.Forms;
+
+namespace TicTacToe.ViewModels
+{
+    public class TicTacToeViewModel : BaseViewModel
+    {
+        private readonly ISetSymbolService _service;
+
+        private bool? _leftTop;
+        private bool? _centerTop;
+        private bool? _rightTop;
+
+        private bool? _leftCenter;
+        private bool? _centerCenter;
+        private bool? _centerRight;
+
+        private bool? _leftDown;
+        private bool? _centerDown;
+        private bool? _rightDown;
+
+        private readonly bool? isTic;
+
+        public TicTacToeViewModel()
+        {
+            _service = new SetSymbolService();
+
+            this.ChooseSymbolCommand = new Command<string>((key) =>
+            {
+                ChooseActionAsync();
+            });
+        }
+
+        public ICommand ChooseSymbolCommand { protected set; get; }
+
+        //public string LeftTop
+        //{
+        //    get { return _leftTop; }
+        //    set
+        //    {
+        //        if (_previousSymbol != value)
+        //        {
+        //            _previousSymbol = value;
+        //            OnPropertyChanged("LeftTop");
+        //        }
+        //    }
+        //}
+
+        private async Task ChooseActionAsync(string title = "Choose your symbol", string cancel = "Are you sure you want to exit?", string destructive="")
+        {
+            await UserDialogs.Instance?.ActionSheetAsync(title, cancel, destructive, "x", "o"); 
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/Views/TicTacToePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace TicTacToe.Views
+{
+    public partial class TicTacToePage : ContentPage
+    {
+        public TicTacToePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
